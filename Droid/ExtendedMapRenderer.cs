b8f74041d233dfdea360bae12c41af08c0009040
PKwong9 <parrykwong9@hotmail.com>
2017-02-21 15:40:57 +0800

    Add files via upload

---
+++ Droid/ExtendedMapRenderer.cs
@@ -0,0 +1,37 @@
+﻿using Android.Gms.Maps;
+using CoffeeDiary.Android;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.Android;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CoffeeDiary.ExtendedMap), typeof(ExtendedMapRenderer))]
+
+namespace CoffeeDiary.Android
+{
+	public class ExtendedMapRenderer : MapRenderer, IOnMapReadyCallback
+	{
+		private GoogleMap _map;
+
+		public void OnMapReady(GoogleMap googleMap)
+		{
+			_map = googleMap;
+			if (_map != null)
+				_map.MapClick += googleMap_MapClick;
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+		{
+			if (_map != null)
+				_map.MapClick -= googleMap_MapClick;
+			base.OnElementChanged(e);
+			if (Control != null)
+				((MapView)Control).GetMapAsync(this);
+		}
+
+		private void googleMap_MapClick(object sender, GoogleMap.MapClickEventArgs e)
+		{
+			((ExtendedMap)Element).OnTap(new Position(e.Point.Latitude, e.Point.Longitude));
+		}
+	}
+}
