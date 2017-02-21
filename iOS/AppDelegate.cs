bee6d0e23d9464c8ef3ac39f95bd9cd8f3ab148f
PKwong9 <parrykwong9@hotmail.com>
2017-02-21 15:47:26 +0800

    Add files via upload

---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExtendedMap.iOS;
+using Foundation;
+using UIKit;
+
+namespace CoffeeDiary.iOS
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+
+			global::Xamarin.Forms.Forms.Init();
+
+			Xamarin.FormsMaps.Init();
+
+			LoadApplication(new App());
+
+			new ExtendedMapRenderer();
+
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
+
---
+++ iOS/ExtendedMapRenderer.cs
@@ -0,0 +1,50 @@
+﻿using CoreGraphics;
+using ExtendedMap.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.iOS;
+using Xamarin.Forms.Platform.iOS;
+using CoreLocation;
+using MapKit;
+using System.Diagnostics;
+
+[assembly: ExportRenderer(typeof(CoffeeDiary.ExtendedMap), typeof(ExtendedMapRenderer))]
+
+namespace ExtendedMap.iOS
+{
+	public class ExtendedMapRenderer : MapRenderer
+	{
+		private readonly UITapGestureRecognizer _tapRecogniser;
+
+		public ExtendedMapRenderer()
+		{
+			_tapRecogniser = new UITapGestureRecognizer(OnTap)
+			{
+				NumberOfTapsRequired = 1,
+				NumberOfTouchesRequired = 1
+			};
+		}
+
+		private void OnTap(UITapGestureRecognizer recognizer)
+		{
+			var cgPoint = recognizer.LocationInView(Control);
+			var location = ((MKMapView)Control).ConvertPoint(cgPoint, Control);
+			var position = new Position(location.Latitude, location.Longitude);
+
+			((CoffeeDiary.ExtendedMap)Element).OnTap(position);
+			CoffeeDiary.MapPage.TriggerTapEventOnMap(position);
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+		{
+			if (Control != null)
+				Control.RemoveGestureRecognizer(_tapRecogniser);
+			base.OnElementChanged(e);
+			if (Control != null)
+				Control.AddGestureRecognizer(_tapRecogniser);
+		}
+
+
+	}
+}
